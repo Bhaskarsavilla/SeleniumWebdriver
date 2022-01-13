4e6687b851b4381f4ea0773813d4b9ab741b1e9e
Bhaskarsavilla <94519917+Bhaskarsavilla@users.noreply.github.com>
2022-01-13 13:14:11 +0530

    Add files via upload

---
+++ HandleFrames.java
@@ -0,0 +1,63 @@
+package Webdriverpart1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HandleFrames {
+WebDriver driver=null;
+	@SuppressWarnings("deprecation")
+	@BeforeMethod
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\bhask\\OneDrive\\Desktop\\workspace\\SeleniumTest\\driver\\chromedriver.exe");
+		driver= new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://jqueryui.com/");
+		
+		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	}
+	
+	
+	@AfterMethod
+	public void browser() {
+		driver.close();
+	}
+	
+@Test	
+public void Iframe() {
+	
+driver.findElement(By.linkText("Button")).click();
+
+WebElement   title=driver.findElement(By.className("entry-title"));
+Assert.assertEquals(title.getText(), "Button");
+	
+
+driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
+
+WebElement insideframe=driver.findElement(By.xpath("//button[@class='ui-button ui-corner-all ui-widget']"));
+
+Assert.assertEquals(insideframe.getText(), "A button element");
+	
+	insideframe.click();
+	
+	try {
+		Thread.sleep(10000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	
+}
+	
+}
